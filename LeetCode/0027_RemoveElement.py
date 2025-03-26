749ca1c83430ef9a68a5a7d9ca5669ed47f0810d
Hyunwoo Oh <conch.lulls-0e@icloud.com>
2025-03-25 21:48:23 -0400

    add 0026, 0026

---
+++ LeetCode/0027_RemoveElement.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        for i in range(len(nums)):
+            if nums[0]!=val:
+                nums.append(nums[0])
+                del nums[0]
+            else:
+                del nums[0]
+
+        return len(nums)
