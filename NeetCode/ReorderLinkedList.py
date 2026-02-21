492ff794e907ace3b7c7114195cbaa6ac5b32772
Hyunwoo Oh <conch.lulls-0e@icloud.com>
2026-02-21 13:31:10 -0500

    practice on 2026/2/20

---
+++ NeetCode/ReorderLinkedList.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        fast = head
+        slow = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        curr = slow.next
+        slow.next = None
+
+        prev = None
+        while curr:
+            next_node = curr.next
+
+            curr.next = prev
+            prev, curr = curr, next_node
+
+        first, second = head, prev
+
+        while second:
+            tmp1, tmp2 = first.next, second.next
+
+            first.next = second
+            second.next = tmp1
+
+            first, second = tmp1, tmp2
