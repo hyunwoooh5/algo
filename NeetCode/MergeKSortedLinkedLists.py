9d9ea53892072f9edd2951dccb4fb7455631a0f4
Hyunwoo Oh <conch.lulls-0e@icloud.com>
2026-02-28 22:30:19 -0500

    practice on 2026/2/28

--- NeetCode/MergeKSortedLinkedLists.py
+++ NeetCode/MergeKSortedLinkedLists.py
@@ -95,3 +95,43 @@ class Solution:
         curr.next = list1 or list2
 
         return dummy.next
+
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None
+
+        if len(lists) == 1:
+            return lists[0]
+
+        while len(lists) > 1:
+            temp = []
+
+            for i in range(0, len(lists), 2):
+                list1 = lists[i]
+                list2 = lists[i+1] if i+1 < len(lists) else None
+                temp.append(self.mergeTwoLists(list1, list2))
+
+            lists = temp
+
+        return lists[0]
+
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+
+        dummy = ListNode()
+        curr = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+
+            curr = curr.next
+
+        curr.next = list1 or list2
+
+        return dummy.next
--- NeetCode/SortColors.py
+++ NeetCode/SortColors.py
@@ -17,3 +17,23 @@ class Solution:
                 high -= 1
             else:
                 mid += 1
+
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left, right = 0, len(nums)-1
+        mid = 0
+
+        while mid <= right:
+            if nums[mid] == 0:
+                nums[mid], nums[left] = nums[left], nums[mid]
+                left += 1
+                mid += 1
+            elif nums[mid] == 2:
+                nums[mid], nums[right] = nums[right], nums[mid]
+                right -= 1
+            else:
+                mid += 1
